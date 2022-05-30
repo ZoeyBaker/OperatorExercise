48df81ce9a8094ed13c3e4280f6139ba3666493d
Zoey Baker <zoeybaker625@gmail.com>
2022-05-30 11:13:19 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -16,6 +16,13 @@ namespace OperatorExercise
             var mod = a % b;
 
             Console.WriteLine($"{a}/{b} is {div} remainder {mod} ");
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
                 
 
 
